c2eee08e6280aae05a6f07cc68c3fba02c035947
esmaeil <Esmaeil>
2024-10-03 12:07:34 +0330

    jsonmaker added

--- PreparingData/CentralTendency.py
+++ PreparingData/CentralTendency.py
@@ -1,3 +1,4 @@
+import JsonMaker as jm
 import numpy as np
 from scipy.stats import gmean,hmean,mode
 dataArray = np.load('PreparingData/data.npy')
@@ -6,4 +7,13 @@ mean = np.mean(dataArray)
 modeData = mode(dataArray, keepdims=True)
 geometryMean = gmean(dataArray)
 harmonicMean = hmean(dataArray)
+
+jsonMakerObject = jm.Json("PreparingData/CentralTendency")
+jsonMakerObject.add("medain",medain)
+jsonMakerObject.add("mean",mean)
+jsonMakerObject.add("mode",int(modeData.mode[0]))
+jsonMakerObject.add("modeCount",int(modeData.count[0]))
+jsonMakerObject.add("geometryMean",geometryMean)
+jsonMakerObject.add("harmonicMean",harmonicMean)
+jsonMakerObject.save()
 
--- PreparingData/MeasuresOfVarince.py
+++ PreparingData/MeasuresOfVarince.py
@@ -1,12 +1,21 @@
 import numpy as np
-from scipy.stats import gmean,hmean,mode
+import JsonMaker as jm
 import matplotlib.pyplot as plt
 dataArray = np.load('PreparingData/data.npy')
 minData = np.min(dataArray)
 maxData = np.max(dataArray)
 rangeData = maxData - minData
 standardDeviation = np.std(dataArray)
+
 variance = np.var(dataArray)
+jsonMakerObject = jm.Json("PreparingData/MeasuresOfVarince")
+jsonMakerObject.add("minData",int(minData))
+jsonMakerObject.add("maxData",int(maxData))
+jsonMakerObject.add("rangeData",int(rangeData))
+jsonMakerObject.add("standardDeviation",standardDeviation)
+jsonMakerObject.add("variance",variance)
+jsonMakerObject.save()
+
 AConstant = np.sqrt(1/(2*np.pi*standardDeviation**2))
 Bx = -(dataArray - np.mean(dataArray))**2/(2*standardDeviation**2)
 NoramlDist = AConstant*np.exp(Bx)
